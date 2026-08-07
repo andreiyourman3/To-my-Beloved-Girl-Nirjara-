ab02ce8896c72c5a8feb480a060613e8b52dddbb
andreiyourman3 <andreitabulo@gmail.com>
2026-08-07 23:38:52 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("giftBtn");
+
+btn.addEventListener("click", () => {
+    alert("Happy Birthday, My Beloved Girl Nirjara ❤️\n\nThe real surprise is coming soon... 🌅🌹");
+});
