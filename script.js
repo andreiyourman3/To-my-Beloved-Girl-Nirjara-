aa772644cb286ce67c09753f4d7c768053f03f40
andreiyourman3 <andreitabulo@gmail.com>
2026-08-09 09:41:42 +0800

    Remove message display and heart animation

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-function showMessage() {
-    document.getElementById("message").style.display = "block";
-}
 
-setInterval(() => {
-    let heart = document.createElement("div");
-    heart.className = "heart";
-    heart.innerHTML = "❤️";
-
-    heart.style.left = Math.random() * 100 + "vw";
-
-    document.body.appendChild(heart);
-
-    setTimeout(() => {
-        heart.remove();
-    }, 5000);
-
-}, 500);
